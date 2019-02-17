973e9dfff5100c4cd18cb990b21c99de28c6603e
unknown <am1560857@gmail.com>
2019-02-17 11:30:55 -0800

    add details Screen

--- lib/presentation/ArticlesDetailsFragment.dart
+++ lib/presentation/ArticlesDetailsFragment.dart
@@ -1,14 +1,15 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:ny_times_most_pupulararticles/Constants.dart';
 import 'package:ny_times_most_pupulararticles/data/models/Article.dart';
 
 class ArticlesDetailsFragment extends StatefulWidget {
   Article article;
-  ArticlesDetailsFragment({this.article});
+
+  ArticlesDetailsFragment({@required this.article});
+
   @override
-  _ArticlesDetailsFragmentState createState() => _ArticlesDetailsFragmentState();
+  _ArticlesDetailsFragmentState createState() =>
+      _ArticlesDetailsFragmentState();
 }
 
 class _ArticlesDetailsFragmentState extends State<ArticlesDetailsFragment> {
@@ -16,32 +17,62 @@ class _ArticlesDetailsFragmentState extends State<ArticlesDetailsFragment> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverAppBar(
-              expandedHeight: 200.0,
-              floating: false,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                  title: Text(APPBAR_TITLE,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      )),
-                  background: Image.network(
-                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
-                    fit: BoxFit.cover,
-                  )),
-            ),
-          ];
-        },
-        body: Center(
-          child: Text("Sample Text"),
-        ),
-      ),
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                expandedHeight: 200.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    title: Text(APPBAR_TITLE,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        )),
+                    background: Image.network(
+                      widget.article.mediaMetaDataList[4].url,
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ];
+          },
+          body: buildBody()),
     );
   }
 
+  Widget buildBody() {
+    return Column(
+      children: <Widget>[
+        Text(widget.article.title,
+            style: TextStyle(color: Colors.black, fontSize: 18)),
+        SizedBox(
+          height: 16,
+        ),
+        Text(widget.article.description, style: TextStyle(fontSize: 16)),
+        SizedBox(
+          height: 8,
+        ),
+        buildCreatedByRow(CREATED_BY, widget.article.writtenBy),
+        SizedBox(
+          height: 4,
+        ),
+        buildCreatedByRow(SOURCE, widget.article.source)
+      ],
+    );
+  }
 
-
+  Widget buildCreatedByRow(String title, String description) {
+    return Row(
+      children: <Widget>[
+        Text(
+          title,
+          style: TextStyle(color: Colors.green, fontSize: 18),
+        ),
+        Text(
+          description,
+          style: TextStyle(fontSize: 18),
+        )
+      ],
+    );
+  }
 }
