138d56adfdf2a189c9bc3d180f38313fd8ae44b0
ashaker <ashaker@egypt.itgsolutions.com>
2019-02-17 17:05:58 +0200

    Update ArticlesDetailsFragment.dart

--- lib/presentation/ArticlesDetailsFragment.dart
+++ lib/presentation/ArticlesDetailsFragment.dart
@@ -1,8 +1,12 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:ny_times_most_pupulararticles/Constants.dart';
+import 'package:ny_times_most_pupulararticles/data/models/Article.dart';
 
 class ArticlesDetailsFragment extends StatefulWidget {
+  Article article;
+  ArticlesDetailsFragment({this.article});
   @override
   _ArticlesDetailsFragmentState createState() => _ArticlesDetailsFragmentState();
 }
@@ -10,6 +14,34 @@ class ArticlesDetailsFragment extends StatefulWidget {
 class _ArticlesDetailsFragmentState extends State<ArticlesDetailsFragment> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  title: Text(APPBAR_TITLE,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      )),
+                  background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    fit: BoxFit.cover,
+                  )),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
+        ),
+      ),
+    );
   }
+
+
+
 }
