d70e04204bae112a1357f692a6f2d8e9b7e152f8
ashaker <ashaker@egypt.itgsolutions.com>
2019-02-17 15:10:59 +0200

    Create ArticlesDetailsFragment.dart

---
+++ lib/presentation/ArticlesDetailsFragment.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:flutter/material.dart';
+
+class ArticlesDetailsFragment extends StatefulWidget {
+  @override
+  _ArticlesDetailsFragmentState createState() => _ArticlesDetailsFragmentState();
+}
+
+class _ArticlesDetailsFragmentState extends State<ArticlesDetailsFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
